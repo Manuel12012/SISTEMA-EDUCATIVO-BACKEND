b7932f099e3345caddec8c09eced731137358968
Manuel Anthony Meza Rivas <manuelmezarivas120@gmail.com>
2026-04-04 18:50:06 -0500

    .

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../config.php';
 
 // Cabeceras CORS
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5173"); // tu frontend local
+header("Access-Control-Allow-Origin: http://localhost:5174"); // tu frontend local
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // Si usas cookies o tokens de sesión:
