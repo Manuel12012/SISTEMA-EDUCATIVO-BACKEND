a2630b6da9e8afe542a9983e6f96f532804a81ca
Manuel Anthony Meza Rivas <manuelmezarivas120@gmail.com>
2026-04-04 18:36:41 -0500

    .

--- public/index.php
+++ public/index.php
@@ -4,11 +4,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
 // Cabeceras CORS
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173"); // tu frontend local
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// ⚠ Si la petición es OPTIONS, terminar aquí
+// Responder a OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
