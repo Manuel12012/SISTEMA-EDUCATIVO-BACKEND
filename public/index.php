6e729099ba66f8230ad4a66097f5867bc3aa5afd
Manuel Anthony Meza Rivas <manuelmezarivas120@gmail.com>
2026-04-04 15:41:11 -0500

    .

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo "DEBUG INICIO<br>";
+echo "INICIO OK<br>";
 define('BASE_PATH', dirname(__DIR__));
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
